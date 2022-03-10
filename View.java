cb512b6bb122d5a255a6993f3c8521f6445705cf
SofiLam13 <87708580+SofiLam13@users.noreply.github.com>
2022-03-10 07:58:44 -0600

    Se lee el archivo cards_desc.txt

---
+++ View.java
@@ -0,0 +1,7 @@
+public class View {
+    public static void main(String[] args) {
+    archivos a= new archivos();
+    a.ReadCards();
+    }
+    
+}
--- archivos.java
+++ archivos.java
@@ -1,9 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Universidad del valle de Guatemala
  * Hoja de Trabajo 6
  * Manuel Martínez 
  * Sofía Lam
  */
+
 public class archivos {
-    
+
+    public Map<String, String> ReadCards(){
+        Map<String, String> map= new HashMap<String, String>();
+        try {
+            FileReader fr = new FileReader("cards_desc.txt");
+            BufferedReader bf = new BufferedReader(fr);
+            String linea;
+            while ((linea = bf.readLine()) != null) {
+            String[] cadenas = linea.split("|");
+            map.put(cadenas[0], cadenas[1]);
+            }
+            fr.close();
+            System.out.println("El archivo se ha leído correctamente");
+            } catch (FileNotFoundException e) {
+            System.out.println("Archivo no encontrado");
+            e.printStackTrace();
+            } catch (IOException e) {
+            System.out.println("Error de E/S");
+            e.printStackTrace();
+            }
+        return map;
+            
+    }
 }
